beb207991bed3a7265013b6cec3733b7bc29c77f
thegoatboat <110457070+thegoatboat@users.noreply.github.com>
2022-10-04 12:19:47 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "./App.css";
 import Profile from "./profile/Profile";
-
 function App() {
   const handleClick = () => {
     alert("Hello@welcom.byby");
@@ -13,9 +12,16 @@ function App() {
         bio="Lorem"
         profession="student"
         handleClick={handleClick}
-      />
+      >
+        <img
+          className="photo"
+          src="https://i.pinimg.com/originals/20/60/4d/20604d135e8cc98bd168614250c09b17.png"
+          alt="img"
+        />
+      </Profile>
     </div>
   );
 }
 
 export default App;
+
